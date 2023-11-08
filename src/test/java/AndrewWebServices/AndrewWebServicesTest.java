4dbeb50629c35d0c2d07715198d7a9143fd22fbf
Axel Sun <axelsun@cmu.edu>
2023-11-07 19:58:44 -0500

    Test with test doubles

--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -3,6 +3,8 @@ package AndrewWebServices;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import static org.mockito.Mockito.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,9 +17,9 @@ public class AndrewWebServicesTest {
     @Before
     public void setUp() {
         // You need to use some mock objects here
-        database = new Database(); // We probably don't want to access our real database...
-        recommender = new RecSys();
-        promoService = new PromoService();
+        database = new InMemoryDatabase(); // We probably don't want to access our real database...
+        recommender = mock(RecSys.class);
+        promoService = mock(PromoService.class);
 
         andrewWebService = new AndrewWebServices(database, recommender, promoService);
     }
@@ -31,6 +33,7 @@ public class AndrewWebServicesTest {
     @Test
     public void testGetRecommendation() {
         // This is taking way too long to test
+        when(recommender.getRecommendation("Scotty")).thenReturn("Animal House");
         assertEquals("Animal House", andrewWebService.getRecommendation("Scotty"));
     }
 
@@ -38,11 +41,16 @@ public class AndrewWebServicesTest {
     public void testSendEmail() {
         // How should we test sendEmail() when it doesn't have a return value?
         // Hint: is there something from Mockito that seems useful here?
+        doNothing().when(promoService).mailTo("Scotty");
+        andrewWebService.sendPromoEmail("Scotty");
+        verify(promoService).mailTo("Scotty");
     }
 
     @Test
     public void testNoSendEmail() {
         // How should we test that no email has been sent in certain situations (like right after logging in)?
         // Hint: is there something from Mockito that seems useful here?
+        assertTrue(andrewWebService.logIn("Scotty", 17214));
+        verify(promoService, never()).mailTo(anyString());
     }
 }
